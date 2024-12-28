c96e4fb893781b5f897c2acc98fcb2fcc0346436
zakadamirli <z3kkadamirli@gmail.com>
2024-12-29 01:31:20 +0400

    feat: Pull initializeNewCart method

--- src/main/java/com/dailycodework/dreamshops/service/cart/ICartService.java
+++ src/main/java/com/dailycodework/dreamshops/service/cart/ICartService.java
@@ -2,6 +2,7 @@ package com.dailycodework.dreamshops.service.cart;
 
 
 import com.dailycodework.dreamshops.model.Cart;
+import com.dailycodework.dreamshops.model.User;
 
 import java.math.BigDecimal;
 
@@ -12,7 +13,7 @@ public interface ICartService {
 
     BigDecimal getTotalPrice(Long id);
 
-    Long initializeNewCart();
+    Cart initializeNewCart(User user);
 
     Cart getCartByUserId(Long userId);
 }
