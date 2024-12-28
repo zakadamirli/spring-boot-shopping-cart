73dff8c47d1035b5efa0df360a59d64ebfe23b08
zakadamirli <z3kkadamirli@gmail.com>
2024-12-28 23:22:05 +0400

    feat: add dtoConverter

--- src/main/java/com/dailycodework/dreamshops/service/user/IUserService.java
+++ src/main/java/com/dailycodework/dreamshops/service/user/IUserService.java
@@ -1,5 +1,6 @@
 package com.dailycodework.dreamshops.service.user;
 
+import com.dailycodework.dreamshops.dto.UserDto;
 import com.dailycodework.dreamshops.model.User;
 import com.dailycodework.dreamshops.request.CreateUserRequest;
 import com.dailycodework.dreamshops.request.UserUpdateRequest;
@@ -9,4 +10,6 @@ public interface IUserService {
     User createUser(CreateUserRequest userRequest);
     User updateUser(UserUpdateRequest request, Long userId);
     void deleteUser(Long userId);
+
+    UserDto convertUserToDto(User user);
 }
