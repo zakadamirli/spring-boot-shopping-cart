72636bb9f44ffb68a094550acee377ea2e4df77b
zakadamirli <z3kkadamirli@gmail.com>
2024-12-28 22:44:54 +0400

    feat: add create user endpoint |UserController

--- src/main/java/com/dailycodework/dreamshops/controller/UserController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.dailycodework.dreamshops.controller;
 
+import com.dailycodework.dreamshops.exceptions.AlreadyExistsException;
 import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
 import com.dailycodework.dreamshops.model.User;
+import com.dailycodework.dreamshops.request.CreateUserRequest;
 import com.dailycodework.dreamshops.response.ApiResponse;
 import com.dailycodework.dreamshops.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
@@ -29,5 +28,13 @@ public class UserController {
         }
     }
 
-
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request) {
+        try {
+            User user = userService.createUser(request);
+            return ResponseEntity.ok(new ApiResponse("Create User Success!", user));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
