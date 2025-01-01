d11755041442c4f271a7fd76f2c3108556695bf8
zakadamirli <z3kkadamirli@gmail.com>
2025-01-01 14:20:14 +0400

    feat: add custom jpa query for existing product

--- src/main/java/com/dailycodework/dreamshops/repository/ProductRepository.java
+++ src/main/java/com/dailycodework/dreamshops/repository/ProductRepository.java
@@ -20,4 +20,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByBrandAndName(String brand, String name);
 
     Long countByBrandAndName(String brand, String name);
+
+    boolean existsByNameAndBrand(String name, String brand);
 }
