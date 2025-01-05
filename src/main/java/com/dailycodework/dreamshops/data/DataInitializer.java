4e5cf995efde932913244766c7370bfd6ed12079
zakadamirli <z3kkadamirli@gmail.com>
2025-01-05 23:36:01 +0400

    feat: inject PasswordEncoder dependency, add createDefaultAdmin method in the class

--- src/main/java/com/dailycodework/dreamshops/data/DataInitializer.java
+++ src/main/java/com/dailycodework/dreamshops/data/DataInitializer.java
@@ -1,27 +1,42 @@
 package com.dailycodework.dreamshops.data;
 
+import com.dailycodework.dreamshops.model.Role;
 import com.dailycodework.dreamshops.model.User;
 import com.dailycodework.dreamshops.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
+@Transactional
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
+    public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
         createDefaultUserIfNotExists();
+        createDefaultRoleIfNotExists(defaultRoles);
+        createDefaultAdminIfNotExists();
+
     }
 
     private void createDefaultUserIfNotExists() {
-        for (int i = 0; i < 5; i++) {
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+
+        for (int i = 1; i <= 5; i++) {
             String defaultEmail = "user" + i + "@gmail.com";
 
             if (userRepository.existsByEmail(defaultEmail)) {
@@ -31,10 +46,38 @@ public class DataInitializer implements ApplicationListener<ApplicationReadyEven
             user.setFirstName("The User");
             user.setLastName("User " + i);
             user.setEmail(defaultEmail);
-            user.setPassword("password");
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
 
             log.warn("Default set user {} created successfully!", i);
         }
+    }
+
+    private void createDefaultAdminIfNotExists() {
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
+
+        for (int i = 1; i <= 2; i++) {
+            String defaultEmail = "admin" + i + "@gmail.com";
+
+            if (userRepository.existsByEmail(defaultEmail)) {
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("Admin " + i);
+            user.setEmail(defaultEmail);
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(adminRole));
+            userRepository.save(user);
+
+            log.warn("Default admin user {} created successfully!", i);
+        }
+    }
+
+    private void createDefaultRoleIfNotExists(Set<String> roles) {
+        roles.stream()
+                .filter(role -> roleRepository.findByName(role).isEmpty())
+                .map(Role::new).forEach(roleRepository::save);
     }
 }
