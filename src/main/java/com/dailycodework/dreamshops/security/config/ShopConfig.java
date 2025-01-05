3649514de20f87a7d7b130d51b77e8f2ceb00563
zakadamirli <z3kkadamirli@gmail.com>
2025-01-05 22:12:43 +0400

    feat: add all needed config @Bean-s

--- src/main/java/com/dailycodework/dreamshops/security/config/ShopConfig.java
+++ src/main/java/com/dailycodework/dreamshops/security/config/ShopConfig.java
@@ -1,11 +1,27 @@
 package com.dailycodework.dreamshops.security.config;
 
+import com.dailycodework.dreamshops.security.jwt.AuthTokenFilter;
+import com.dailycodework.dreamshops.security.jwt.JwtAuthEntryPoint;
+import com.dailycodework.dreamshops.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class ShopConfig {
+
+    private final ShopUserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint authEntryPoint;
+
 
     @Bean
     public ModelMapper modelMapper() {
@@ -14,5 +30,28 @@ public class ShopConfig {
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
         return modelMapper;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration  authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
