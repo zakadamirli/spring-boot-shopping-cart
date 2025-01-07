38b190f3d761f3ff5bcb50e83a74817a4eb27792
zakadamirli <z3kkadamirli@gmail.com>
2025-01-07 19:51:09 +0400

    fix: clean code

--- src/main/java/com/dailycodework/dreamshops/controller/UserController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 
         try {
             User user = userService.getUserById(userId);
-            UserDto userDto= userService.convertUserToDto(user);
+            UserDto userDto = userService.convertUserToDto(user);
             return ResponseEntity.ok(new ApiResponse("Success", userDto));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
@@ -35,7 +35,7 @@ public class UserController {
     public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request) {
         try {
             User user = userService.createUser(request);
-            UserDto userDto= userService.convertUserToDto(user);
+            UserDto userDto = userService.convertUserToDto(user);
             return ResponseEntity.ok(new ApiResponse("Create User Success!", userDto));
         } catch (AlreadyExistsException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
@@ -47,7 +47,7 @@ public class UserController {
 
         try {
             User user = userService.updateUser(request, userId);
-            UserDto userDto= userService.convertUserToDto(user);
+            UserDto userDto = userService.convertUserToDto(user);
             return ResponseEntity.ok(new ApiResponse("Update User Success!", userDto));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
