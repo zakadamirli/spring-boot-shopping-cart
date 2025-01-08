d0002d6948f7282abe881508843205b859077246
zakadamirli <z3kkadamirli@gmail.com>
2025-01-08 22:55:04 +0400

    feat: impl LoginRequest class

--- src/main/java/com/dailycodework/dreamshops/request/LoginRequest.java
+++ src/main/java/com/dailycodework/dreamshops/request/LoginRequest.java
@@ -1,4 +1,14 @@
 package com.dailycodework.dreamshops.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
 }
