481c11c2a39567b6d806640737d100174c1c28b3
zakadamirli <z3kkadamirli@gmail.com>
2025-01-25 22:14:36 +0400

    feat: update code

--- src/main/java/com/dailycodework/dreamshops/data/RoleRepository.java
+++ src/main/java/com/dailycodework/dreamshops/data/RoleRepository.java
@@ -4,7 +4,6 @@ import com.dailycodework.dreamshops.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String role);
--- src/main/java/com/dailycodework/dreamshops/security/config/ShopConfig.java
+++ src/main/java/com/dailycodework/dreamshops/security/config/ShopConfig.java
@@ -31,7 +31,8 @@ public class ShopConfig {
     private final ShopUserDetailsService userDetailsService;
     private final JwtAuthEntryPoint authEntryPoint;
 
-    private static final List<String> SECURED_URLS = List.of("/api/v1/carts/**", "/api/v1/cartItems/**", "/api/v1/products/**", "/api/v1/auth/**");
+    private static final List<String> SECURED_URLS = List.of("/api/v1/carts/**",
+            "/api/v1/cartItems/**", "/api/v1/products/**", "/api/v1/auth/**");
 
 
     @Bean
