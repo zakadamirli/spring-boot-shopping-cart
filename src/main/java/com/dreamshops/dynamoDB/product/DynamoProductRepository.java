f23f50c711ef8077b290edff3e71974c87e6aaf0
kamalshabanow <kamalshabanov04@gmail.com>
2025-03-13 18:40:52 +0400

    Integrated dynamodb for product and cart table

---
+++ src/main/java/com/dreamshops/dynamoDB/product/DynamoProductRepository.java
@@ -0,0 +1,41 @@
+package com.dreamshops.dynamoDB.product;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class DynamoProductRepository {
+
+    private final DynamoDBMapper dynamoDBMapper;
+
+    public String createProduct(DynamoProduct dynamoProduct) {
+        dynamoDBMapper.save(dynamoProduct);
+        return dynamoProduct.getId();
+    }
+
+    public DynamoProduct getProductById(String id) {
+        return dynamoDBMapper.load(DynamoProduct.class, id);
+    }
+
+    public DynamoProduct updateProduct(String id, DynamoProduct dynamoProduct) {
+        DynamoProduct load = dynamoDBMapper.load(DynamoProduct.class, id);
+
+        load.setName(dynamoProduct.getName());
+        load.setBrand(dynamoProduct.getBrand());
+        load.setPrice(dynamoProduct.getPrice());
+        load.setInventory(dynamoProduct.getInventory());
+        load.setDescription(dynamoProduct.getDescription());
+        load.setCategory(dynamoProduct.getCategory());
+
+        return dynamoDBMapper.load(DynamoProduct.class, id);
+    }
+
+    public String deleteProduct(String id) {
+        DynamoProduct load = dynamoDBMapper.load(DynamoProduct.class, id);
+        dynamoDBMapper.delete(load);
+
+        return load.getId() + " get deleted";
+    }
+}
