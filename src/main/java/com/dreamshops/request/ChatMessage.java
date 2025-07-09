e56e563cce5cd4b7891387030a913d7d31fa1534
zakadamirli <z3kkadamirli@gmail.com>
2025-07-10 03:13:46 +0400

    feat: create chat message dto

---
+++ src/main/java/com/dreamshops/request/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.dreamshops.request;
+
+public class ChatMessage {
+    private String sender;
+    private String content;
+    private String timestamp;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
+
